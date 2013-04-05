5c89bcf3bf79f74fb3fb5132f1d60f5ff84ecd0c
Paul Melnikow <email@paulmelnikow.com>
2013-04-05 16:52:51 -0400

    Initial commit

---
+++ AFHTTPClient+Synchronous.m
@@ -0,0 +1,141 @@
+// AFHTTPClient+Synchronous.m
+//
+// Copyright (c) 2013 Paul Melnikow
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFHTTPClient+Synchronous.h"
+#import "AFHTTPRequestOperation.h"
+
+NSString * const AFHTTPClientErrorDomain = @"com.alamofire.httpclient";
+NSInteger const AFHTTPClientBackgroundTaskExpiredError = -1001;
+
+@implementation AFHTTPClient (Synchronous)
+
++ (dispatch_queue_t)sharedCallbackQueue {
+    static dispatch_queue_t queue;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        queue = dispatch_queue_create("com.alamofire.networking.httpclient_synchronous.callbacks", DISPATCH_QUEUE_CONCURRENT);
+    });
+    
+    return queue;
+}
+
+- (id)synchronouslyPerformMethod:(NSString *) method
+                            path:(NSString *) path
+                      parameters:(NSDictionary *)parameters
+                       operation:(AFHTTPRequestOperation *__autoreleasing *) operationPtr
+                           error:(NSError *__autoreleasing *) outError
+{
+    __block id result = nil;
+    
+    dispatch_group_t group = dispatch_group_create();
+    
+    NSURLRequest *request = [self requestWithMethod:method path:path parameters:parameters];
+    AFHTTPRequestOperation *op = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (operationPtr) *operationPtr = operation;
+        result = responseObject;
+        dispatch_group_leave(group);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (operationPtr) *operationPtr = operation;
+        if (outError) *outError = error;
+        dispatch_group_leave(group);
+    }];
+    
+    // Since the caller will wait until the callbacks finish executing, we need to deliver
+    // these callbacks on a queue which is not the caller's.
+    op.successCallbackQueue = op.failureCallbackQueue = [self.class sharedCallbackQueue];
+    
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+    // By registering the operation as a long-running background task, if the app enters
+    // background, it is more likely to complete. However, it does not guarantee that the
+    // method will return.
+    //
+    // When a background task is about to run out of time, the expiration handler is run
+    // on the main queue.
+    //
+    // Using this feature while blocking the main thread causes a deadlock. Then the
+    // operation never gets to call `endBackgroundTask:` and the system terminates the
+    // app. Refer to the docs for
+    // `-[AFURLConnectionOperation setShouldExecuteAsBackgroundTaskWithExpirationHandler:`
+    // and `-[NSOperation beginBackgroundTaskWithExpirationHandler:]`
+    
+    if (![NSThread isMainThread]) {
+        [op setShouldExecuteAsBackgroundTaskWithExpirationHandler:^(void) {
+            if (outError) *outError =
+                [NSError errorWithDomain:AFHTTPClientErrorDomain
+                                    code:AFHTTPClientBackgroundTaskExpiredError
+                                userInfo:@{ NSLocalizedDescriptionKey: @"Background operation time expired" }];
+            dispatch_group_leave(group);
+        }];
+        // At this point this thread may wake up but probably isn't guaranteed to do so.
+    }
+#endif
+    
+    dispatch_group_enter(group);
+    [op start];
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+#if !OS_OBJECT_USE_OBJC
+    dispatch_release(group);
+#endif
+    return result;
+}
+
+- (id)synchronouslyGetPath:(NSString *)path
+                parameters:(NSDictionary *)parameters
+                 operation:(AFHTTPRequestOperation *__autoreleasing *)operationPtr
+                     error:(NSError *__autoreleasing *)outError
+{
+    return [self synchronouslyPerformMethod:@"GET" path:path parameters:parameters operation:operationPtr error:outError];
+}
+
+- (id)synchronouslyPostPath:(NSString *)path
+                 parameters:(NSDictionary *)parameters
+                  operation:(AFHTTPRequestOperation *__autoreleasing *) operationPtr
+                      error:(NSError *__autoreleasing *) outError
+{
+    return [self synchronouslyPerformMethod:@"POST" path:path parameters:parameters operation:operationPtr error:outError];
+}
+
+- (id)synchronouslyPutPath:(NSString *)path
+                parameters:(NSDictionary *)parameters
+                 operation:(AFHTTPRequestOperation *__autoreleasing *) operationPtr
+                     error:(NSError *__autoreleasing *) outError
+{
+    return [self synchronouslyPerformMethod:@"PUT" path:path parameters:parameters operation:operationPtr error:outError];
+}
+
+- (id)synchronouslyDeletePath:(NSString *)path
+                   parameters:(NSDictionary *)parameters
+                    operation:(AFHTTPRequestOperation *__autoreleasing *) operationPtr
+                        error:(NSError *__autoreleasing *) outError
+{
+    return [self synchronouslyPerformMethod:@"DELETE" path:path parameters:parameters operation:operationPtr error:outError];
+}
+
+- (id)synchronouslyPatchPath:(NSString *)path
+                  parameters:(NSDictionary *)parameters
+                   operation:(AFHTTPRequestOperation *__autoreleasing *) operationPtr
+                       error:(NSError *__autoreleasing *) outError
+{
+    return [self synchronouslyPerformMethod:@"PATCH" path:path parameters:parameters operation:operationPtr error:outError];
+}
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 AFNetworking-Synchronous
 ========================
 
-Synchronous requests for AFNetworking
+A minimal category which extends [AFNetworking][], specifically
+`AFHTTPClient`, to support synchronous requests.
+
+For Mac OS and iOS. (Tested on Mac OS)
+
+# Example
+
+````
+NSError *error = nil;
+NSData *result =
+[[AFHTTPClient sharedClient] synchronouslyGetPath:@"/document/123"
+                                       parameters:nil
+                                        operation:NULL
+                                            error:&error];
+````
+
+# Discussion
+
+## First, consider adopting an asynchronous design
+ 
+Before you decide to use this category, consider whether you can adopt an asynchronous design instead. As @mattt wrote, asynchronism a tough thing to
+get your head around, but it's well worth the mental overhead. Rather than creating methods that fetch and return network data, use blocks or delegate methods to call back with the results when you have them.
+ 
+Using the asynchronous API has many advantages:
+
+ - When you start an operation on the main thread, you return control to the run loop immediately, so your UI can remains responsive. Blocking the main thread for a long time is never a good idea. "Be responsive," Appe urges in the OS X user experience guidelines. Asynchronous network operations allow
+ you to do that.
+ - AFNetworking makes asynchronous code easy to write and easy to read. With block-based success and failure handlers, you don't need to implement
+ delegate protocols or provide selectors for callbacks.
+ - AFNetworking and Grand Central Dispatch take care of threading for you, so your code does not need to manage threads, run selectors in the background,
+ or invoke dispatch_async. Your completion blocks will be executed on the main thread (unless you configure the operations otherwise).
+ - You can provide a better user experience while waiting for a response. Networks are unreliable, particularly for mobile users, and servers can be bogged down. Your users' experiences will be better if you design for a slow connection, which you can only do asynchronously.
+ 
+However, in some cases, a synchronous response is better, such as when the document architecture or another framework is handling the multithreading for you, and expects a synchronous result. This code attempts to provide a safe and reliable way to use the framework synchronously.
+ 
+While it overrides the default success and failure queues to avoid a deadlock, it can't anticipate every possible situation. In particular, you should not set the queue from which you're invoking as the processing queue, which will cause a deadlock.
+ 
+## The Main Thread
+ 
+You shouldn't call these methods from the main thread. On iOS, if your application enters the background while one of these methods is running on the main thread, a deadlock may result and your application could be terminated.
+
+[AFNetworking]: https://github.com/AFNetworking/AFNetworking
